16389292294c5e29ee5dd32c088c464a7a226813
marzepanh <marzepanh@gmail.com>
2021-03-25 11:31:01 +0300

    finished launcher

--- src/main/java/archiver/ArchiverLauncher.java
+++ src/main/java/archiver/ArchiverLauncher.java
@@ -1,9 +1,9 @@
 package archiver;
 
 import org.kohsuke.args4j.Argument;
-import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
+import java.io.IOException;
 
 public class ArchiverLauncher {
 
@@ -19,19 +19,28 @@ public class ArchiverLauncher {
     @Argument(metaVar = "InputName", required = true, usage = "Input file name")
     private String inputFileName;
 
+//Command Line: pack-rle [-z|-u] [-out outputname.txt] inputname.txt
     public static void main(String[] args) {
         new ArchiverLauncher().launch(args);
     }
 
     private void launch(String[] args) {
         CmdLineParser parser = new CmdLineParser(this);
-
         try {
             parser.parseArgument(args);
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.err.println(e.getMessage());
         }
 
-
+        Archiver rle = new Archiver();
+        try {
+            if (outputFileName == null) outputFileName = inputFileName + "_result";
+            if (pack) rle.encode(inputFileName, outputFileName);
+                else rle.decode(inputFileName, outputFileName);
+            if (pack) System.out.println("Successful packing");
+                else System.out.println("Successful unpacking");
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
--- src/test/java/archiver/ArchiverTest.java
+++ src/test/java/archiver/ArchiverTest.java
@@ -1,13 +1,25 @@
 package archiver;
 
 import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class ArchiverTest {
 
     @Test
-    public void encode() {
-        //assertEquals("3A-1B3A-4DCDA", RLE.encode("AAABAAADCDA"));
-        //assertEquals("4825-4-31333a28", RLE.encode("888855-3133aaa88"));
+    public void archiver() throws IOException {
+        Archiver pack = new Archiver();
+        File in = new File("files/input.txt");
+        pack.encode("files/input.txt", "files/output.txt");
+        pack.decode("files/output.txt", "files/result.txt");
+        File out = new File("files/result.txt");
+        List<String> c1 = Files.readAllLines(in.toPath());
+        List<String> c2 = Files.readAllLines(out.toPath());
+        assertTrue(c1.containsAll(c2));
     }
 }
